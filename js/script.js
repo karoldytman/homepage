7edff4dc9647d71d9c182155c59b32a85bb0a117
Karol Dytman <dytmankarol@wp.pl>
2022-12-26 11:37:40 +0100

    formatting js

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@
         const mainBody = document.querySelector(".mainBody")
         const js__button = document.querySelector(".js__button");
         mainBody.classList.toggle("color")
-
+       
         if (mainBody.classList.contains("color")) {
             js__button.innerText = "OFF";
         } else {
             js__button.innerText = "ON";
         }
-
+       
     };
 
     const init = () => {
